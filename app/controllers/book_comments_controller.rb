c6a46caf26a535296926bd59376488c3f627816f
nishimototo <85095171+nishimototo@users.noreply.github.com>
2021-07-25 22:26:38 +0900

    Update book_comments_controller.rb

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -3,7 +3,7 @@ class BookCommentsController < ApplicationController
     @book = Book.find(params[:book_id])
     @book_comment = current_user.book_comments.new(book_comment_params)
     @book_comment.book_id = @book.id
-    if @comment.save
+    if @book_comment.save
       redirect_to book_path(@book)
     else
       @user = @book.user
